48a81701b3ba10e49385d2702db851258ace9216
BRUNA FIUSA <bruna.fiusa@senacsp.edu.br>
2025-11-06 16:24:57 -0300

    Feat: emplementado a model Tarefa. #1

--- src/app.ts
+++ src/app.ts
@@ -2,33 +2,8 @@ import express from "express";
 import { Request, Response, NextFunction } from "express";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get("/parametro/:nome", (req: Request, res: Response, next: NextFunction) => {
-  const nome = req.params.nome;
-  console.log("Rota de parametro - Cliente digitou: ", nome);
-  res.send(`Você digitou o nome: ${nome}`);
-});
 
-app.get("/query", (req: Request, res: Response, next: NextFunction) => {
-  const nome = req.query.nome;
-  console.log("Cliente digitou: ", nome);
-  res.send(`Você digitou o nome: ${nome}`);
-});
-
-app.get("/corpo", (req: Request, res: Response, next: NextFunction) => {
-  const nome = req.body.nome;
-  console.log("Variavel dentro do corpo- Cliente digitou: ", nome);
-  res.send(`Você digitou o nome: ${nome}`);
-});
-
-app.get("/query", (req: Request, res: Response, next: NextFunction) => {
-  console.log("Aconteceu algo!");
-  res.send(`Rodou tudo serto`);
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.send("Erro");
-});
 
 export default app;
---
+++ src/models/tarefa.ts
@@ -0,0 +1,13 @@
+class Tarefa{
+  id: number;
+  nome: string;
+  descricao: string;
+
+  private static proximoId = 1;
+
+  constructor(nome: string, descricao:string){
+    this.id = Tarefa.proximoId++;
+    this.nome = nome;
+    this.descricao = descricao;
+  }
+}
