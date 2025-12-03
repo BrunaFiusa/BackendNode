71ecf45336aad1b1b31ca85ec6f87b8a4290a31b
BRUNA FIUSA <bruna.fiusa@senacsp.edu.br>
2025-12-03 16:51:00 -0300

    Teste

--- src/tests/tarefa.test.ts
+++ src/tests/tarefa.test.ts
@@ -1,8 +1,9 @@
+/*Classe de testes*/
+
 test("GET: /tarefas = 200", async() =>{
   const res = await fetch("http://localhost:3000/tarefas/")
   expect(res.status).toBe(200)
 
   const body = await res.json()
   expect(Array.isArray(body)).toBe(true)
-
 })
